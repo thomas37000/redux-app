452c0816caa3a3c58d2b66414480654bbddb7c0d
thomas <thomas.chalanson@sfr.fr>
2022-02-11 14:27:56 +0100

    import the useSelector from redux

--- src/components/Profil.jsx
+++ src/components/Profil.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Profil = () => {
+  // avec Redux useSelector remplace useState 
+  const user = useSelector((state) => state.user.value);
   return (
     <div>
-      <div>Name :</div>
-      <div>Email : </div>
-      <div>Age : </div>
+      <div>Name :{user.name}</div>
+      <div>Email :{user.email} </div>
+      <div>Age : {user.age}</div>
     </div>
   );
 };
